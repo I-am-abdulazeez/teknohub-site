c1047e175417651f55838e426b2440f43dade6bf
AbdulAzeez <abdulazeezolamide50@gmail.com>
2026-02-12 14:16:36 +0100

    chore: add gridPattern to About page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -2,6 +2,7 @@ import type { Route } from "./+types/about";
 
 import { Link } from "react-router";
 import { Button } from "@heroui/button";
+import { GridPattern } from "~/components/patterns";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -22,7 +23,7 @@ export default function About() {
       {/* Hero Section */}
       <section className="relative bg-linear-to-br from-[#0B2B5D] to-[#0a1f42] text-white py-24 overflow-hidden">
         <div className="absolute inset-0 opacity-10">
-          <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjAiIGhlaWdodD0iNjAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PHBhdHRlcm4gaWQ9ImdyaWQiIHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCIgcGF0dGVyblVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHBhdGggZD0iTSAxMCAwIEwgMCAwIDAgMTAiIGZpbGw9Im5vbmUiIHN0cm9rZT0id2hpdGUiIHN0cm9rZS13aWR0aD0iMSIvPjwvcGF0dGVybj48L2RlZnM+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgZmlsbD0idXJsKCNncmlkKSIvPjwvc3ZnPg==')]"></div>
+          <GridPattern />
         </div>
         <div className="container mx-auto px-4 relative z-10">
           <div className="max-w-4xl mx-auto text-center">
